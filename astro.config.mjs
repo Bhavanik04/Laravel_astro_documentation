b74dbe846a5d01a0637fe496a9bf6560f6a89641
Bhavani K <bhavani.k@praniontech.com>
2024-07-02 12:30:16 +0530

    deploy commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,47 +5,40 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import starlightDocSearch from '@astrojs/starlight-docsearch';
 // import { markdown } from '@astrojs/markdown-remark';
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "http://localhost:3000",
-  
   integrations: [starlight({
     title: 'My Docs',
     // social: {
     //   github: 'https://github.com/withastro/starlight'
     // },
-    plugins: [
-      starlightDocSearch({
-        appId: 'YOUR_APP_ID',
-        apiKey: 'YOUR_SEARCH_API_KEY',
-        indexName: 'YOUR_INDEX_NAME',
-      }),
-    ],
+    plugins: [starlightDocSearch({
+      appId: 'YOUR_APP_ID',
+      apiKey: 'YOUR_SEARCH_API_KEY',
+      indexName: 'YOUR_INDEX_NAME'
+    })],
     logo: {
-      src: '/var/www/html/shipper-laravel-project/src/assets/avatar.svg',
+      src: '/var/www/html/shipper-laravel-project/src/assets/avatar.svg'
       // replacesTitle: true
     },
-    sidebar: [
-   
-    { label: 'Laravel & Package composition', link: '/package/package/' },
-    {
-
+    sidebar: [{
+      label: 'Laravel & Package composition',
+      link: '/package/package/'
+    }, {
       label: 'API Security',
-      items: [
-      {
+      items: [{
         label: 'API Security',
         link: '/apisecurity/token/'
-      },
-     
-    ]
+      }]
     }, {
       label: 'API Logging',
       autogenerate: {
         directory: 'apilogging'
-      },
-      
-    },
-    {
+      }
+    }, {
       label: 'API Documentation',
       autogenerate: {
         directory: 'apidoc/apidoc'
@@ -59,12 +52,10 @@ export default defineConfig({
       autogenerate: {
         directory: 'apidoc'
       }
-    }
-  ]
-  }), tailwind(),
-  mdx(),
-  sitemap(),
+    }]
+  }), tailwind(), mdx(), sitemap()
   // markdown(),
-
-]
+  ],
+  output: "server",
+  adapter: vercel()
 });
